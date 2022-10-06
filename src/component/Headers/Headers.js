5bdd1c5630eac4dbc0d296aa5ee56469a44a6126
Md Ismail Ahammed Roman <ismailahmmedroman@gmail.com>
2022-10-06 22:47:08 +0600

    Header add

--- src/component/Headers/Headers.js
+++ src/component/Headers/Headers.js
@@ -4,8 +4,7 @@ const Headers = () => {
     return (
      
         <div  className='Header text-center flex justify-center bg-gray-400'>
-           <img className='h-14' src={logo} alt="" />
-        <h1 className='mt-3 font-bold text-2xl'><samp className='text-[#ffffff]'>Click</samp><samp className='text-[#fc960f]'>Zone</samp><samp className=''>-Gymn</samp><samp className='text-[#fc960f]'>asiume</samp></h1>
+     
         
         </div>
     );
